b6f2acac2a0a150b71e9320cb931babc016dd585
Addict7 <nicolasfloquet@gmail.com>
2012-08-10 19:48:52 +0200

    GLES 2.0

--- puyopuyo-desktop/src/com/tacoid/puyopuyo/PuyoPuyoDesktop.java
+++ puyopuyo-desktop/src/com/tacoid/puyopuyo/PuyoPuyoDesktop.java
@@ -5,6 +5,6 @@ import com.badlogic.gdx.backends.jogl.JoglApplication;
 public class PuyoPuyoDesktop {
 	public static void main(String[] argv) {
 		//new JoglApplication(new SuperFlu(), "SuperFlu", 800, 480, false);
-		new JoglApplication(PuyoPuyo.getInstance(), "Puyo Puyo", 1024, 600, false);
+		new JoglApplication(PuyoPuyo.getInstance(), "Puyo Puyo", 1248, 768, false);
 	}
 }
