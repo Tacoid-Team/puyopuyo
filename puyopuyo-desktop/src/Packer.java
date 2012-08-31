b029f2927b80b7149871b0720f5b5038dc0f6eea
Addict7 <nicolasfloquet@gmail.com>
2012-08-31 18:15:10 +0200

    Packer Planks

--- puyopuyo-desktop/src/Packer.java
+++ puyopuyo-desktop/src/Packer.java
@@ -6,5 +6,6 @@ public class Packer {
     public static void main (String[] args) {
         TexturePacker2.process("images/puyos", "images/puyos", "pages");
         TexturePacker2.process("images/controls", "images/controls", "pages");
+        TexturePacker2.process("images/menu/plank-fr", "images/menu/plank-fr", "pages");
     }
 }
