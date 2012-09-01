f293c2df23d8105735308fabdda3919f257d5d52
Maxime Chéramy <maxime81@gmail.com>
2012-09-01 13:20:42 +0200

    Meilleure disposition de puyos en mode solo.

--- puyopuyo-android/src/com/tacoid/puyopuyo/GameSoloScreen.java
+++ puyopuyo-android/src/com/tacoid/puyopuyo/GameSoloScreen.java
@@ -34,8 +34,8 @@ public class GameSoloScreen implements Screen {
 		gameLogic = new GameLogic();
 		controller = new Controller(gameLogic, stage);
 
-		gridActor = new GridActor(gameLogic, 280, 320, 75, 70);
-		NextPieceActor nextPieceActor = new NextPieceActor(gameLogic, 80, 920, 70);
+		gridActor = new GridActor(gameLogic, 280, 330, 70, 64);
+		NextPieceActor nextPieceActor = new NextPieceActor(gameLogic, 80, 920, 64);
 		ScoreActor scoreActor = new ScoreActor(gameLogic, 590, 1230);
 
 		TextureRegion backgroundRegion 	= new TextureRegion(PuyoPuyo.getInstance().manager.get("images/fond_solo.png",Texture.class), VIRTUAL_WIDTH, VIRTUAL_HEIGHT);
