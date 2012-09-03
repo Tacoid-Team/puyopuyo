81511f3ecb1fce48c5810255c1b46088fe74b50d
Maxime Chéramy <maxime81@gmail.com>
2012-09-03 14:08:04 +0200

    Nouveau puyo vert.

--- puyopuyo-android/src/com/tacoid/puyopuyo/GameScreen.java
+++ puyopuyo-android/src/com/tacoid/puyopuyo/GameScreen.java
@@ -75,11 +75,11 @@ public class GameScreen implements Screen {
 		elapsedTime = 0;
 
 		gridActor = new GridActor(gameLogic, 214, 26, 54, 48);
-		NextPieceActor nextPieceActor = new NextPieceActor(gameLogic, 55, 480, 48);
+		NextPieceActor nextPieceActor = new NextPieceActor(gameLogic, 45, 500, 48);
 		ScoreActor scoreActor = new ScoreActor(gameLogic, 480, 730);
 
 		gridActorIA = new GridActor(gameLogicIA, 728, 26, 54, 48);
-		NextPieceActor nextPieceActorIA = new NextPieceActor(gameLogicIA, 1100,	480, 48);
+		NextPieceActor nextPieceActorIA = new NextPieceActor(gameLogicIA, 1100,	500, 48);
 		ScoreActor scoreActorIA = new ScoreActor(gameLogicIA, 890, 730);
 		
 
