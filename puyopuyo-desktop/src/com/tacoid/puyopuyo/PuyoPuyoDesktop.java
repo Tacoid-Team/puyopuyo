4913dbee9ce0024c2ab46ada1a5b578931e927d6
Addict7 <nicolasfloquet@gmail.com>
2012-11-09 18:51:03 +0100

    Oubli

--- puyopuyo-desktop/src/com/tacoid/puyopuyo/PuyoPuyoDesktop.java
+++ puyopuyo-desktop/src/com/tacoid/puyopuyo/PuyoPuyoDesktop.java
@@ -1,10 +1,13 @@
 package com.tacoid.puyopuyo;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.tacoid.tracking.TrackingManager;
+import com.tacoid.tracking.TrackingManager.TrackerType;
 
 public class PuyoPuyoDesktop {
 	public static void main(String[] argv) {
 		PuyoPuyo.getInstance().setDesktopMode(true);
+		TrackingManager.setTrackerType(TrackerType.DUMMY);
 		new LwjglApplication(PuyoPuyo.getInstance(), "Puyo Puyo",  1280, 768, false);
 	}
 }
