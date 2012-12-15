ae54d2c7bdbd12dd3b81988a906f94716b849f8a
Maxime Chéramy <maxime81@gmail.com>
2012-12-15 15:53:56 +0100

    Revert "Revert "Désactivation admod et analytics le temps de dev.""
    This reverts commit 136410721c287040c4e10051c3a1e28e2cebb018.

--- puyopuyo-android/src/com/tacoid/pweek/PweekAndroid.java
+++ puyopuyo-android/src/com/tacoid/pweek/PweekAndroid.java
@@ -105,7 +105,8 @@ public class PweekAndroid extends AndroidApplication implements IActivityRequest
 		    View gameView = initializeForView(Pweek.getInstance(), config);
 		
 		    // Cr�ation de la vu adMob
-		    adView = new AdView(this, AdSize.BANNER, "a150a3f124cd8c4"); // Put in your secret key here
+//		    adView = new AdView(this, AdSize.BANNER, "a150a3f124cd8c4"); // Put in your secret key here
+		    adView = new AdView(this, AdSize.BANNER, "aaaaaaaaaaaaaaa"); // Put in your secret key here
 		    adView.loadAd(new AdRequest());
 		    adView.setAdListener(this);
 
