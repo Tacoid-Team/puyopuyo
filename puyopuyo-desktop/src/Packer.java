70e78523d8fa2870c670533452d5344d73273bc5
Maxime Chéramy <maxime81@gmail.com>
2013-01-11 22:03:18 +0100

    Déplacement images. ref #81

--- puyopuyo-desktop/src/Packer.java
+++ puyopuyo-desktop/src/Packer.java
@@ -4,12 +4,12 @@ import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
 
 public class Packer {
     public static void main (String[] args) {
-        TexturePacker2.process("images/puyos", "images/puyos", "pages");
-        TexturePacker2.process("images/controls", "images/controls", "pages");
-        TexturePacker2.process("images/menu/plank-fr", "images/menu/plank-fr", "pages");
-        TexturePacker2.process("images/menu/plank-en", "images/menu/plank-en", "pages");
-        TexturePacker2.process("images/panels/landscape", "images/panels/landscape", "pages");
-        TexturePacker2.process("images/panels/portrait", "images/panels/portrait", "pages");
-        TexturePacker2.process("images/bouttons", "images/bouttons", "pages");
+        TexturePacker2.process("images_src/puyos", "images/puyos", "pages");
+        TexturePacker2.process("images_src/controls", "images/controls", "pages");
+        TexturePacker2.process("images_src/menu/plank-fr", "images/menu/plank-fr", "pages");
+        TexturePacker2.process("images_src/menu/plank-en", "images/menu/plank-en", "pages");
+        TexturePacker2.process("images_src/panels/landscape", "images/panels/landscape", "pages");
+        TexturePacker2.process("images_src/panels/portrait", "images/panels/portrait", "pages");
+        TexturePacker2.process("images_src/bouttons", "images/bouttons", "pages");
     }
 }
